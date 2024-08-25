838ec777ea492b623ca3dbff641b8615a59c7383
Murillo Daigder <murillo.daigder@edu.unifil.br>
2024-08-25 18:36:23 -0300

    commit dos arquivos

--- server.js
+++ server.js
@@ -1,33 +1,51 @@
-const express = require( 'express');
-const axios = require('axios')
+const { Endereco } = require('./models');
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-
+// Rota para a raiz do site
 app.get('/', (req, res) => {
-    res.send ('Hello world');
+    res.send('Hello world');
 });
 
-app.get ('/consulta-cep/:cep', async (req, res) => { 
-const cep = req.params.cep; // Obtendo o CEP da URL
-const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
+// Rota para consulta de CEP
+app.get('/consulta-cep/:cep', async (req, res) => { 
+    const cep = req.params.cep;
+    const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
     
     if (!cepRegex.test(cep)) {
-    res.status(400).send('CEP inválido. Formato: XXXXX-XXX');
-    return;
+        return res.status(400).send(`CEP inválido. Formato: XXXXX-XXX`);
     }
-    try{
-    // Fazendo a requisição para a API do ViaCEP
-    const response = await axios.get (`https://viacep.com.br/ws/${cep}/json/`);
-    res. json(response.data); // Retorna os dados da resposta
 
-    }catch (error) {
-    console.error ('Erro ao fazer requisição:", error');
-    res.status (500). send('Erro ao consultar o CEP');
-}
+    try {
+        const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
+        const enderecoData = response.data;
+
+        if (enderecoData.erro) {
+            return res.status(404).send('CEP não encontrado.');
+        }
+
+        const novoEndereco = await Endereco.create({
+            cep: enderecoData.cep.replace('-', ''),
+            logradouro: enderecoData.logradouro,
+            numero: null,
+            complemento: enderecoData.complemento || '',
+            bairro: enderecoData.bairro,
+            cidade: enderecoData.localidade,
+            estado: enderecoData.uf,
+            municipioIBGE: enderecoData.ibge
+        });
+
+        res.status(201).json(enderecoData);
+    } catch (error) {
+        console.error('Erro ao fazer requisição:', error);
+        res.status(500).send('Erro ao consultar o CEP.');
+    }
 });
 
-app. listen(port,()=> {
-    console.log('Servidor rodando em http://localhost:5000');
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
